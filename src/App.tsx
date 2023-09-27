388822437889fbbbeb4d2d8e66bbb140c2f7d570
Jeremy.K <38066205+jekhoxy@users.noreply.github.com>
2023-09-27 09:59:38 +0000

    Vite + React + Fiber + Threejs init project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Physical Based Rendering (PBR)</h3>
+      <h4>Box geomtry with lights</h4>
+      
+      
+    </>
+  )
+}
+
+export default App
